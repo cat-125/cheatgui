7bc7adb9ad51411ca9c1e877b0fe2e7ee82198a1
Cat0125 <106539381+Cat0125@users.noreply.github.com>
2024-01-07 17:57:01 +0300

    Fix build + change version in package.json

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 from css_html_js_minify import css_minify
 from rjsmin import jsmin
+import os
 
 INJ_TEMPLATE = '{js};cheatgui.utils.includeCSS(`{css}`)'
 
@@ -7,6 +8,14 @@ INJ_TEMPLATE = '{js};cheatgui.utils.includeCSS(`{css}`)'
 def read(path: str):
     with open(path, 'r') as f:
         return f.read()
+
+
+def create_directory(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory: ' + directory)
 
 
 def write(path: str, text: str):
@@ -20,6 +29,8 @@ def main():
 
     mjs = jsmin(js)
     mcss = css_minify(css)
+
+    create_directory('build')
 
     write('build/cheatgui.min.js', mjs)
     write('build/cheatgui.min.css', mcss)
