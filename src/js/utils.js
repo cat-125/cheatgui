876d51f8a636bc70e797405cf71e69f3249274d1
Cat125 <106539381+cat-125@users.noreply.github.com>
2024-03-08 14:48:52 +0300

    Use Webpack (#2)
    * use webpack
    
    * use more webpack features
    
    * split into more files and move config functions
    
    * update utils.js
    
    * use `countDigitsAfterDecimal`

---
+++ src/js/utils.js
@@ -0,0 +1,247 @@
+/**
+ * Useful utilites
+ * @module
+ */
+
+import {
+	GUIElement,
+	Widget,
+	Text,
+	Button,
+	Input,
+	NumberInput,
+	Slider,
+	Switch,
+	Dropdown,
+	Tree,
+	Container,
+	Row
+} from './cheatgui.js';
+
+/**
+ * Shortcut for `parent.querySelector(selector)`.
+ * @param {any} selector - CSS selector
+ * @param {any} [parent=document] - Parent element
+ * @returns {HTMLElement | null}
+ * @public
+ */
+export function $(selector, parent = document) {
+	if (typeof selector !== 'string') return selector;
+	return $(parent).querySelector(selector);
+}
+
+/**
+ * Create an HTML element
+ * @param {string} type - HTML element 
+ * @returns {HTMLElement}
+ * @public
+ */
+export function createElem(type) {
+	return document.createElement(type);
+}
+
+/**
+ * Get the distance between two points
+ * @param {number} x1
+ * @param {number} y1
+ * @param {number} x2
+ * @param {number} y2
+ * @returns {number}
+ * @public
+ */
+export function distance(x1, y1, x2, y2) {
+	const a = x1 - x2;
+	const b = y1 - y2;
+	return Math.sqrt(a * a + b * b);
+}
+
+/**
+ * Generate a random string
+ * @param {number} length
+ * @param {string} _chars - List of characters to use in the random string
+ * @returns {string}
+ * @public
+ */
+export function generateId(length, _chars = '') {
+	const chars = _chars || 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	let result = '';
+	for (let i = 0; i < length; i++) {
+		result += chars[Math.floor(Math.random() * chars.length)];
+	}
+	return result;
+}
+
+/**
+ * Clamp a value between a minimum and a maximum
+ * @param {number} val
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ * @public
+ */
+export function clamp(val, min, max) {
+	return Math.max(min, Math.min(val, max));
+}
+
+/**
+ * Convert a value between a minimum and a maximum to a percentage
+ * @param {number} val
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ * @public
+ */
+export function range2percentage(val, min, max) {
+	return 100 / (max - min) * val;
+}
+
+/**
+ * Snap a value to the nearest multiple of a step
+ * @param {number} value
+ * @param {number} step
+ * @returns {number}
+ * @public
+ */
+export function snap(value, step) {
+	if (step === 0) {
+		throw new Error("Step cannot be zero");
+	}
+
+	const remainder = value % step;
+	let result;
+
+	if (remainder <= step / 2) {
+		result = value - remainder;
+	} else {
+		result = value + step - remainder;
+	}
+
+	return result;
+}
+
+/**
+ * Get the number of digits after the decimal point
+ * @param {number} number
+ * @returns {number}
+ * @deprecated use getNumberOfDigitsAfterPeriod instead
+ * @public
+ */
+export function getNumberOfDigitsAfterPeriod(number) {
+	let stringNumber = number.toString();
+	let parts = stringNumber.split(".");
+
+	if (parts.length > 1) {
+		return parts[1].length;
+	}
+	return 0;
+}
+
+/**
+ * Get the number of digits after the decimal point
+ * @param {number} number
+ * @returns {number}
+ * @public
+ */
+export function countDigitsAfterDecimal(number) {
+	const numberString = number.toString();
+	const indexOfDecimal = numberString.indexOf('.');
+
+	if (indexOfDecimal === -1) return 0;
+	return numberString.length - indexOfDecimal - 1;
+}
+
+/**
+ * Get the name of a widget
+ * @param {GUIElement} widget
+ * @returns {string}
+ * @public
+ */
+export function getWidgetName(widget) {
+	if (typeof widget == 'string' || widget instanceof Text) {
+		return 'text';
+	} else if (widget instanceof Button) {
+		return 'button'
+	} else if (widget instanceof Input) {
+		return 'input';
+	} else if (widget instanceof NumberInput) {
+		return 'number-input';
+	} else if (widget instanceof Switch) {
+		return 'switch';
+	} else if (widget instanceof Slider) {
+		return 'slider';
+	} else if (widget instanceof Dropdown) {
+		return 'dropdown';
+	} else if (widget instanceof Tree) {
+		return 'tree';
+	} else if (widget instanceof Container) {
+		return 'container';
+	} else if (widget instanceof Row) {
+		return 'row';
+	} else if (typeof widget.view !== 'undefined') {
+		return 'has-view';
+	} else if (widget instanceof Widget) {
+		return 'widget'
+	} else if (widget instanceof GUIElement) {
+		return 'gui-element';
+	} else {
+		return 'unknown';
+	}
+}
+
+/**
+ * Append a widget to the body
+ * @param {Widget} widget
+ * @public
+ */
+export function appendToBody(widget) {
+	document.body.appendChild(widget.getRef());
+}
+
+/**
+ * Include a CSS code
+ * @param {string} css
+ * @public
+ */
+export function includeCSS(css) {
+	const head = document.head;
+	const style = createElem('style');
+	style.setAttribute('type', 'text/css');
+	style.innerHTML = css;
+	head.appendChild(style);
+}
+
+/**
+ * Include a CSS link
+ * @param {string} url
+ * @public
+ */
+export function includeCSSLink(url) {
+	const link = createElem('link');
+	link.rel = 'stylesheet';
+	link.href = url;
+	document.head.appendChild(link);
+}
+
+/**
+ * Include a JS file
+ * @param {string} url
+ * @public
+ */
+export function includeJS(url) {
+	const script = createElem('script');
+	script.src = url;
+	document.body.appendChild(script);
+}
+
+/**
+ * Load a CSS theme
+ * @param {string} url
+ * @public
+ */
+export function loadTheme(url) {
+	const link = $(`link#cgui-theme`, document.head) ?? createElem('link');
+	link.id = 'cgui-theme'
+	link.rel = 'stylesheet';
+	link.href = url;
+	document.head.appendChild(link);
+}
