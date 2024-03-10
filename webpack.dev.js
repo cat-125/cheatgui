397ba1051768bd30e65c1ca8915b689383c812cd
Cat125 <106539381+cat-125@users.noreply.github.com>
2024-03-10 19:39:58 +0300

    change devtool

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,5 +3,5 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
 	mode: 'development',
-	devtool: 'eval-cheap-module-source-map'
+	devtool: 'cheap-module-source-map'
 });
